24866ab0203648c54afe348dfca445671542cb29
gaji12345 <gajimax12345@gmail.com>
2024-07-05 17:59:46 +0530

    backend item save implement

--- BackEnd/src/servlets/ItemServlet.java
+++ BackEnd/src/servlets/ItemServlet.java
@@ -15,7 +15,9 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
@@ -84,6 +86,56 @@ public class ItemServlet extends HttpServlet {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code=  req.getParameter("itemCode");
+        String name=  req.getParameter("itemDesc");
+        String prce=   req.getParameter("itemPrice");
+        String qty=   req.getParameter("itemQty");
+
+        System.out.println(code+" "+ name+ " "+prce+" " +qty);
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType ( "application/json");
+
+        resp.addHeader("Access-Control-Allow-Origin","*");
+
+        try {
+
+            Connection connection = ds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("insert into item values (?,?,?,?)");
+            pstm.setObject(1, code);
+            pstm.setObject(2, name);
+            pstm.setObject(3, prce);
+            pstm.setObject(4, qty);
+            boolean b = pstm.executeUpdate() > 0;
+
+//
+
+            if (b) {
+                //resp.setStatus(200);
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);//browser is read
+                objectBuilder.add("status", 200);
+                objectBuilder.add("message", "Successfully Added");
+                objectBuilder.add("data", "");
+                writer.print(objectBuilder.build());
+
+            }
+            connection.close();
+
+
+        } catch (SQLException throwables) {
+            resp.setStatus(200);
+
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("status",400);
+            objectBuilder.add("messages","Error");
+            objectBuilder.add("data",throwables.getLocalizedMessage());
+            writer.print(objectBuilder.build());
+            throwables.printStackTrace();
         }
     }
 
