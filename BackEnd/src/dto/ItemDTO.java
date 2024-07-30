d8cb69f04f7626c3cf2fca345b98c83a2c9d942e
gaji12345 <gajimax12345@gmail.com>
2024-07-30 19:32:38 +0530

    item dto and entity created

---
+++ BackEnd/src/dto/ItemDTO.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO {
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
+
+}
