52206ae3e42d9aa0fd0d323ce9db0ec805e8196d
gaji12345 <gajimax12345@gmail.com>
2024-07-30 19:34:07 +0530

    item dao impl implement

--- BackEnd/src/dao/custom/impl/ItemDAOIMPL.java
+++ BackEnd/src/dao/custom/impl/ItemDAOIMPL.java
@@ -26,18 +26,48 @@ public class ItemDAOIMPL implements ItemDAO {
     }
 
     @Override
-    public boolean add(Item item) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean add(Item i) throws SQLException, ClassNotFoundException {
+        try(Connection conn = ds.getConnection()){
+            PreparedStatement pstm = conn.prepareStatement("INSERT INTO item VALUES (?,?,?,?)");
+            pstm.setString(1, i.getCode());
+            pstm.setString(2, i.getDescription());
+            pstm.setDouble(3, i.getUnitPrice());
+            pstm.setInt(4, i.getQtyOnHand());
+
+            return pstm.executeUpdate() > 0;
+        }
     }
 
     @Override
-    public boolean update(Item item) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean update(Item i) throws SQLException, ClassNotFoundException {
+        try (Connection conn = ds.getConnection()){
+            PreparedStatement pstm = conn.prepareStatement("UPDATE item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
+            pstm.setString(1, i.getDescription());
+            pstm.setDouble(2, i.getUnitPrice());
+            pstm.setInt(3, i.getQtyOnHand());
+            pstm.setString(4, i.getCode());
+
+            return pstm.executeUpdate() > 0;
+        }
     }
 
     @Override
-    public boolean delete(String string) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean delete(String code) throws SQLException, ClassNotFoundException {
+        try (Connection connection = ds.getConnection();) {
+            PreparedStatement pstm = connection.prepareStatement("delete from item where code=?");
+            pstm.setObject(1, code);
+
+            if (pstm.executeUpdate() > 0) {
+                System.out.println("Deleted");
+                return true;
+            } else {
+                System.out.println("failed");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            return false;
+        }
     }
 
     @Override
